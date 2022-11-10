49c4f15b39a3634d57efe516c87675b445521dc7
Patrick <patrick.a.powers2@gmail.com>
2022-11-10 16:46:15 -0500

    infused into the water

--- Items/Accessories/T2Uncommon/Infusion.cs
+++ Items/Accessories/T2Uncommon/Infusion.cs
@@ -1,5 +1,8 @@
-﻿using Terraria;
+﻿using System;
+using RiskOfTerrain.Content.Accessories;
+using Terraria;
 using Terraria.ID;
+using Microsoft.Xna.Framework;
 
 namespace RiskOfTerrain.Items.Accessories.T2Uncommon
 {
@@ -18,6 +21,32 @@ namespace RiskOfTerrain.Items.Accessories.T2Uncommon
             Item.accessory = true;
             Item.rare = ItemRarityID.Orange;
             Item.value = Item.sellPrice(gold: 2);
+        }
+
+        public int ExtraMaxHp = 0;
+        public int WearOffTimer = 0;
+
+        public override void OnKillEnemy(EntityInfo entity, OnKillInfo info)
+        {
+            if (ExtraMaxHp < 50)
+                ExtraMaxHp++;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.statLifeMax2 += ExtraMaxHp;
+            
+            if (WearOffTimer == 1000)
+            {
+                if (ExtraMaxHp > 0)
+                {
+                    int subAmount = (int)Math.Round(ExtraMaxHp * 0.20) + 1;
+                    ExtraMaxHp -= subAmount;
+                    CombatText.NewText(new Rectangle((int)player.Center.X, (int)player.Center.Y, 3, 3), Color.Yellow, -subAmount, false);
+                }
+                WearOffTimer = 0;
+            }
+            WearOffTimer++;
         }
     }
 }
