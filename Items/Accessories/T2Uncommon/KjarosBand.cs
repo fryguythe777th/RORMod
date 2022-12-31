4546974f43d156f6508efd7e59c5a55902ad6cab
Patrick <patrick.a.powers2@gmail.com>
2022-12-31 14:42:51 -0500

    kjaros band, proto guillotine

--- Items/Accessories/T2Uncommon/KjarosBand.cs
+++ Items/Accessories/T2Uncommon/KjarosBand.cs
@@ -1,4 +1,6 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using RiskOfTerrain.Content.Accessories;
+using Terraria;
 using Terraria.ID;
 
 namespace RiskOfTerrain.Items.Accessories.T2Uncommon
@@ -18,6 +20,14 @@ namespace RiskOfTerrain.Items.Accessories.T2Uncommon
             Item.accessory = true;
             Item.rare = ItemRarityID.LightRed;
             Item.value = Item.sellPrice(gold: 3);
+        }
+
+        public override void OnKillEnemy(EntityInfo entity, OnKillInfo info)
+        {
+            if (info.lastHitDamage >= info.lifeMax * 0.6 && !info.friendly && !info.spawnedFromStatue && info.lifeMax > 5)
+            {
+                Projectile.NewProjectile(entity.GetSource_Accessory(Item), info.Center, Vector2.Zero, ProjectileID.SandnadoFriendly, 0, 0);
+            }
         }
     }
 }
--- Items/Accessories/T2Uncommon/OldGuillotine.cs
+++ Items/Accessories/T2Uncommon/OldGuillotine.cs
@@ -1,4 +1,6 @@
-﻿using Terraria;
+﻿using System.Collections.Generic;
+using RiskOfTerrain.Content.Accessories;
+using Terraria;
 using Terraria.ID;
 
 namespace RiskOfTerrain.Items.Accessories.T2Uncommon
@@ -18,6 +20,23 @@ namespace RiskOfTerrain.Items.Accessories.T2Uncommon
             Item.accessory = true;
             Item.rare = ItemRarityID.Green;
             Item.value = Item.sellPrice(gold: 1, silver: 50);
+        }
+
+        public override void ModifyHit(EntityInfo entity, EntityInfo victim, Entity projOrItem, ref int damage, ref float knockBack, ref bool crit)
+        {
+            NPC npc = Main.npc[victim.entity.whoAmI];
+            npc.GetElitePrefixes(out List<Content.Elites.EliteNPCBase> prefixes);
+
+            if (prefixes.Count > 0 && npc.life <= npc.lifeMax * 0.13)
+            {
+                npc.life = 1;
+                npc.StrikeNPC(npc.lifeMax, 0f, 0);
+                npc.active = false;
+                if (Main.netMode != NetmodeID.SinglePlayer)
+                {
+                    NetMessage.SendData(MessageID.DamageNPC, -1, -1, null, npc.whoAmI, npc.lifeMax);
+                }
+            }
         }
     }
 }
