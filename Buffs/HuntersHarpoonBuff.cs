4c768be3544ad10a73f67dc3d0cb4de548773c49
Patrick <patrick.a.powers2@gmail.com>
2023-01-01 10:42:11 -0500

    hunters harpoon (need a buff sprite)

---
+++ Buffs/HuntersHarpoonBuff.cs
@@ -0,0 +1,26 @@
+﻿using RiskOfTerrain.Items.Accessories.T1Common;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace RiskOfTerrain.Buffs
+{
+    public class HuntersHarpoonBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true;
+        }
+
+        public static void AddStack(Entity entity, int time, int stacksAmt)
+        {
+            if (entity is Player player && stacksAmt < 5)
+                player.AddBuff(ModContent.BuffType<HuntersHarpoonBuff>(), time * stacksAmt);
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.accRunSpeed *= 1.8f;
+        }
+    }
+}
---
+++ Items/Accessories/T2Uncommon/HuntersHarpoon.cs
@@ -0,0 +1,30 @@
+﻿using RiskOfTerrain.Buffs;
+using RiskOfTerrain.Content.Accessories;
+using Terraria;
+using Terraria.ID;
+
+namespace RiskOfTerrain.Items.Accessories.T2Uncommon
+{
+    public class HuntersHarpoon : ModAccessory
+    {
+        public override void SetStaticDefaults()
+        {
+            SacrificeTotal = 1;
+            RORItem.GreenTier.Add(Type);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
+            Item.accessory = true;
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Item.sellPrice(gold: 2);
+        }
+
+        public override void OnKillEnemy(EntityInfo entity, OnKillInfo info)
+        {
+            HuntersHarpoonBuff.AddStack(entity.entity, 120, 1);
+        }
+    }
+}
