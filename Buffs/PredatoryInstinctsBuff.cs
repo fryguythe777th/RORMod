45eee8b4faccd8a4fb30529cd0e653abd3dfa7b3
Patrick <patrick.a.powers2@gmail.com>
2023-01-19 17:25:07 -0500

    my very predatory instincts (can we please hit 1 item sprite and 1 buff sprite for this)

---
+++ Buffs/PredatoryInstinctsBuff.cs
@@ -0,0 +1,20 @@
+﻿using RiskOfTerrain.Items.Accessories.T1Common;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace RiskOfTerrain.Buffs
+{
+    public class PredatoryInstinctsBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetAttackSpeed(DamageClass.Generic) += 0.2f;
+        }
+    }
+}
---
+++ Items/Accessories/T2Uncommon/PredatoryInstincts.cs
@@ -0,0 +1,39 @@
+﻿using RiskOfTerrain.Buffs;
+using RiskOfTerrain.Content.Accessories;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RiskOfTerrain.Items.Accessories.T2Uncommon
+{
+    public class PredatoryInstincts : ModAccessory
+    {
+        public override void SetStaticDefaults()
+        {
+            SacrificeTotal = 1;
+            RORItem.GreenTier.Add(Type);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
+            Item.accessory = true;
+            Item.rare = ItemRarityID.Green;
+            Item.value = Item.sellPrice(gold: 1, silver: 50);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetCritChance(DamageClass.Generic) += 5;
+        }
+
+        public override void ModifyHit(EntityInfo entity, EntityInfo victim, Entity projOrItem, ref int damage, ref float knockBack, ref bool crit)
+        {
+            if (crit && entity.entity is Player player)
+            {
+                player.AddBuff(ModContent.BuffType<PredatoryInstinctsBuff>(), 300);
+            }
+        }
+    }
+}
