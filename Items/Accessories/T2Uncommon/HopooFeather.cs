05bf47696ff3219bb35d5ecd12d21a6f6f1fe599
Patrick <patrick.a.powers2@gmail.com>
2023-02-02 17:54:50 -0500

    this feather hops

---
+++ Items/Accessories/T2Uncommon/HopooFeather.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+
+namespace RiskOfTerrain.Items.Accessories.T2Uncommon
+{
+    public class HopooFeather : ModAccessory
+    {
+        public override void SetStaticDefaults()
+        {
+            SacrificeTotal = 1;
+            RORItem.GreenTier.Add(Type);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
+            Item.accessory = true;
+            Item.rare = ItemRarityID.Green;
+            Item.value = Item.sellPrice(gold: 1, silver: 50);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.noFallDmg = true;
+            player.jumpSpeedBoost *= 1.5f;
+            player.jumpBoost = true;
+        }
+    }
+}
